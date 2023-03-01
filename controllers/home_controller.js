334f87584cda6949981998ebab80d4cc62d931be
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-03-01 16:07:32 +0530

    Authenticate the user while creating a post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,24 @@
-module.exports.home = function(req, res){
+const Post = require('../models/post');
 
-    console.log(req.cookies);
-    res.cookie('Coding',100)
-    return res.render('home', {
-        title: "Home"
-    });
+module.exports.home = function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
 }
 
 // module.exports.actionName = function(req, res){}
