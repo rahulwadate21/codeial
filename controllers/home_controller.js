369aaf5590a60402599dfc9bbc17583e4ef715d9
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-03-06 14:48:14 +0530

    create user profile link in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/post');
-
+const User = require('../models/user');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -20,12 +20,18 @@ module.exports.home = function(req, res){
             path: 'user'
         }
     }).exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
-        });
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users:users
+            });
+        })
+        
     })
 
 }
+
 
 // module.exports.actionName = function(req, res){}
