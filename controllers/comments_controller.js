4658d363a5fae7ca99b2d324c1ecb1fdc5a2a2a5
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-03-25 16:11:42 +0530

    adding mailer and likes to the comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,12 +1,16 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+const Like = require('../models/like');
 
-module.exports.create = async function (req, res) {
+module.exports.create = async function(req, res){
 
-    try {
+    try{
         let post = await Post.findById(req.body.post);
 
-        if (post) {
+        if (post){
             let comment = await Comment.create({
                 content: req.body.content,
                 post: req.body.post,
@@ -15,11 +19,22 @@ module.exports.create = async function (req, res) {
 
             post.comments.push(comment);
             post.save();
+            
+            comment = await comment.populate('user', 'name email').execPopulate();
+            // commentsMailer.newComment(comment);
 
-            if (req.xhr) {
-                // Similar for comments to fetch the user's id!
-                comment = await comment.populate('user', 'name').execPopulate();
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
 
+            })
+
+            if (req.xhr){
+                
+    
                 return res.status(200).json({
                     data: {
                         comment: comment
@@ -33,29 +48,33 @@ module.exports.create = async function (req, res) {
 
             res.redirect('/');
         }
-    } catch (err) {
+    }catch(err){
         req.flash('error', err);
         return;
     }
-
+    
 }
 
 
-module.exports.destroy = async function (req, res) {
+module.exports.destroy = async function(req, res){
 
-    try {
+    try{
         let comment = await Comment.findById(req.params.id);
 
-        if (comment.user == req.user.id) {
+        if (comment.user == req.user.id){
 
             let postId = comment.post;
 
             comment.remove();
 
-            let post = Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // CHANGE :: destroy the associated likes for this comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
+
 
             // send the comment id which was deleted back to the views
-            if (req.xhr) {
+            if (req.xhr){
                 return res.status(200).json({
                     data: {
                         comment_id: req.params.id
@@ -68,13 +87,13 @@ module.exports.destroy = async function (req, res) {
             req.flash('success', 'Comment deleted!');
 
             return res.redirect('back');
-        } else {
+        }else{
             req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    } catch (err) {
+    }catch(err){
         req.flash('error', err);
         return;
     }
-
+    
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const Like = require('../models/like');
 
 module.exports.create = async function(req, res){
     try{
@@ -9,8 +10,8 @@ module.exports.create = async function(req, res){
         });
         
         if (req.xhr){
-            // // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
-            // post = await post.populate('user', 'name').execPopulate();
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
 
             return res.status(200).json({
                 data: {
@@ -39,6 +40,13 @@ module.exports.destroy = async function(req, res){
         let post = await Post.findById(req.params.id);
 
         if (post.user == req.user.id){
+
+            // CHANGE :: delete the associated likes for the post and all its comments' likes too
+            await Like.deleteMany({likeable: post, onModel: 'Post'});
+            await Like.deleteMany({_id: {$in: post.comments}});
+
+
+
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
